1cfcf852793b9ab9f52d1dfd85c3696fb4adbf5f
Benjamin Herson <benjaminherson@gmail.com>
2015-03-06 14:49:31 -0800

    add ability to display individual folder page

--- router.js
+++ router.js
@@ -2,8 +2,12 @@ Bmail.Router.map(function() {
   this.resource('emails', {path: '/'}, function() {
     this.resource('new-email');
   });
+
   this.resource('email', {path: '/email/:email_id'});
+
   this.resource('folders', function() {
     this.resource('new-folder');
   });
+
+  this.resource('folder', {path: '/folder/:folder_id'});
 });
---
+++ routes/folder.js
@@ -0,0 +1,5 @@
+Bmail.FolderRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.store.find('folder', params.folder_id);
+  }
+});
